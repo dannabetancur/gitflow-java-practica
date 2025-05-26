750b04f9c12a0089cf774c789106f0cdfc40c5b4
dannabetancur <dannarojasb221@gmail.com>
2025-05-26 12:28:44 -0500

    Hotfix: corrección en el comentario de la funcionalidad 2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
     // Comentario de la funcionalidad 1
-    // Comentario de la funcionalidad 2
+    // Comentario de la funcionalidad 2 (corregido)
         System.out.println("Hola GitFlow!");
     }
 }
